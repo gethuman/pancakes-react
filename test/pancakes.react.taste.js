cf02a6d7f35fb1362ca844b80fff18d0fbd6f2b7
Jeff Whelpley <jeff@gethuman.com>
2014-11-12 09:58:25 -0500

    Initial checkin

---
+++ test/pancakes.react.taste.js
@@ -0,0 +1,62 @@
+var _               = require('lodash');
+var taste           = require('taste');
+var path            = require('path');
+var pancakes        = require('pancakes');
+var jshint          = require('jshint').JSHINT;
+var prettyjson      = require('prettyjson');
+var fs              = require('fs');
+var dot             = require('dot');
+
+var pancakesReactTaste = _.extend({}, taste);
+
+// universally set strip to false for dot templates (i.e. respect whitespace)
+dot.templateSettings.strip = false;
+
+pancakes.init({
+    preload:        ['utils'],
+    rootDir:        __dirname + '/fixtures',
+    require:        require,
+    container:      'webserver',
+    clientPlugin:   function () {},
+    serverPlugin:   function () {},
+    pluginOptions: {
+        prefix:         'gh',
+        clientType:     'react',
+        ngType:         'factory',
+        transformer:    'basic',
+        appName:        'common'
+    },
+    adapterMap:     {}
+});
+
+/**
+ * eval code with a mock react object
+ * @param code
+ * @param isRaw
+ */
+pancakesReactTaste.validateCode = function validateCode(code, isRaw) {
+    var success = isRaw ?
+        jshint('var blah = ' + code + ';') :
+        jshint(code);
+
+    if (!success) {
+        console.log('\n\nerror with the following generated code:\n\n' + code + '\n');
+        console.log('JSHINT ERRORS\n' + prettyjson.render(jshint.errors));
+    }
+
+    return success;
+};
+
+/**
+ * Get a dot template
+ * @param name
+ * @returns {*}
+ */
+pancakesReactTaste.getTemplate = function getTemplate(name) {
+    var filePath = __dirname + '/../lib/transformers/react.' + name + '.template';
+    var file = fs.readFileSync(path.normalize(filePath));
+    return dot.template(file);
+};
+
+// expose the pancakes react taste object
+module.exports = pancakesReactTaste;
